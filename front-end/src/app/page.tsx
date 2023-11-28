52f2a58392255ddc73374505311f3c96922b00cd
gabryel <valvanogabryel@gmail.com>
2023-11-28 20:53:40 -0300

    feat: :tada: projeto iniciado

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="text-primary-p">pfac</main>;
+}
---
+++ front-end/tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: {
+          50: '#F0FFF4',
+          100: '#C6F6D5',
+          p: '#9AE6B4',
+          300: '#68D391',
+          400: '#48BB78',
+          500: '#38A169',
+          600: '#25855A',
+          700: '#276749',
+          800: '#22543D',
+          900: '#1C4532',
+        },
+        neutral: {
+          50: '#F7FAFC',
+          100: '#EDF2F7',
+          200: '#E2E8F0',
+          300: '#CBD5E0',
+          400: '#A0AEC0',
+          500: '#718096',
+          600: '#4A5568',
+          700: '#2D3748',
+          800: '#1A202C',
+          900: '#171923',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
