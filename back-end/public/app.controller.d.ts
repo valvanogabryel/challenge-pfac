998049bf81336f2a24b0b3090a728b612d433544
gabryel <valvanogabryel@gmail.com>
2023-12-03 12:54:41 -0300

    preparação deploy

--- back-end/public/app.controller.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AppController {
-    sayHello(): Promise<{
-        message: string;
-    }>;
-}
--- back-end/public/dto/create-message.dto.d.ts
+++
@@ -1,3 +0,0 @@
-import { Message } from 'src/messages/entities/message.entity';
-export declare class CreateMessageDto extends Message {
-}
