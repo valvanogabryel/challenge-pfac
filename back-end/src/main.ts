88123b02927cd1a3f22e1757b5abeb0f751017a5
gabryel <valvanogabryel@gmail.com>
2023-12-03 19:57:50 -0300

    update vercel.json

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*', credentials: true });
+  app.use(
+    cors({
+      origin: 'https://challenge-pfac-frontend.vercel.app',
+      credentials: true,
+    })
+  );
+
   await app.listen(process.env.PORT || 4003);
 }
 bootstrap();
