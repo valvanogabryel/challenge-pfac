7ae5022da6c0a02209c411c66302f8072730a0ca
gabryel <valvanogabryel@gmail.com>
2023-12-03 19:37:54 -0300

    atualização da url da api

--- front-end/src/api/index.tsx
+++ front-end/src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4003',
+  baseURL: 'https://challenge-pfac-backend.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
