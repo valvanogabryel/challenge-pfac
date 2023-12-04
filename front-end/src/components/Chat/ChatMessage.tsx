2f486c877af61007b3c1fcdbd2d4b59f1688c7b9
gabryel <valvanogabryel@gmail.com>
2023-12-04 15:41:50 -0300

    adicionado scroll automático nas mensagens

--- front-end/src/components/Chat/ChatMessage.tsx
+++ front-end/src/components/Chat/ChatMessage.tsx
@@ -1,6 +1,7 @@
 import { useUserInfo } from '@/context/userInfoContext';
 import IMessage from '@/interface/IMessage';
 import { motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 interface ChatMessageProps {
   messages: IMessage[];
@@ -8,6 +9,17 @@ interface ChatMessageProps {
 
 export function ChatMessage({ messages }: Readonly<ChatMessageProps>) {
   const userInfo = useUserInfo();
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bottomRef.current) {
+      bottomRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest',
+      });
+    }
+  }, [messages]);
 
   if (messages?.length < 1)
     return (
@@ -17,39 +29,44 @@ export function ChatMessage({ messages }: Readonly<ChatMessageProps>) {
     );
 
   return (
-    <div className="flex overflow-hidden relative h-full">
-      <div className="flex-1 md:pr-20 absolute bottom-0 right-0 w-full">
-        {messages?.map((message, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              message.name === userInfo?.name ? 'justify-end' : 'justify-start'
-            } mb-2`}
-          >
-            <div>
-              <span
-                className={`text-xs text-neutral-400 block m-1 ${
-                  message.name === userInfo?.name ? 'text-end' : 'text-start'
-                }`}
-              >
-                {message.name || null}
-              </span>
-              <motion.div
-                className={`text-neutral-900 py-2 px-4 shadow rounded-xl text-xs md:text-sm select-text ${
-                  message.name === userInfo?.name
-                    ? 'bg-primary-100  rounded-br-none'
-                    : 'bg-primary-50 rounded-bl-none'
-                }`}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.1 }}
-              >
-                {message.text}
-              </motion.div>
+    <div className="flex overflow-y-auto relative h-full">
+      <div className="flex-1 md:pr-20 absolute right-0 w-full">
+        <div className="flex-1 md:pr-20 absolute right-0 w-full">
+          {messages?.map((message, index) => (
+            <div
+              key={index}
+              className={`flex ${
+                message.name === userInfo?.name
+                  ? 'justify-end'
+                  : 'justify-start'
+              } mb-2`}
+            >
+              <div>
+                <span
+                  className={`text-xs text-neutral-400 block m-1 ${
+                    message.name === userInfo?.name ? 'text-end' : 'text-start'
+                  }`}
+                >
+                  {message.name || null}
+                </span>
+                <motion.div
+                  className={`text-neutral-900 py-2 px-4 shadow rounded-xl text-xs md:text-sm select-text ${
+                    message.name === userInfo?.name
+                      ? 'bg-primary-100  rounded-br-none'
+                      : 'bg-primary-50 rounded-bl-none'
+                  }`}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.1 }}
+                >
+                  {message.text}
+                </motion.div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 }
